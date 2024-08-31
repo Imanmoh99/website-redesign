fb7bd088bc2919e3d48f60e08a8c8c6d765c81fa
naolethioware <lemesanaol93@ethioware.org>
2024-08-31 22:58:07 +0300

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,8 +1,1 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDaWyilj9NsON26KSo-cW29fq0s-j0w2o8",
-    authDomain: "authenticate-app-9f92d.firebaseapp.com",
-    projectId: "authenticate-app-9f92d",
-    storageBucket: "authenticate-app-9f92d.appspot.com",
-    messagingSenderId: "811013712823",
-    appId: "1:811013712823:web:4a4ebc4ded8f6755e55632",
-  };
+
